7e9dddafcdf0b520fe6f4b7d9a7018922ae2ca13
Tad Hosford <tad.hosford@gmail.com>
2012-12-23 17:16:41 -0600

    Require files under lib/ recursively rather than explicitly

--- lib/eenie_meenie.rb
+++ lib/eenie_meenie.rb
@@ -1,7 +1,11 @@
 require "eenie_meenie/base"
-require "eenie_meenie/miny_moe"
-require "eenie_meenie/sorters/round_robin"
-require "eenie_meenie/sorters/pure_random"
-require "eenie_meenie/sorters/pick_a_group"
-require "eenie_meenie/version"
-require "eenie_meenie/result"
+#require "eenie_meenie/miny_moe"
+#require "eenie_meenie/sorters/round_robin"
+#require "eenie_meenie/sorters/pure_random"
+#require "eenie_meenie/sorters/pick_a_group"
+#require "eenie_meenie/version"
+#require "eenie_meenie/result"
+
+Dir.glob(File.join(File.dirname(__FILE__), '/**/*.rb')) do |c|
+  require(c)
+end
